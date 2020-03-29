76229b04c9030fd011dbdfcbc72f2d9bbf38d4ce
Mateusz Winicki <M.Winnicki@stud.elka.pw.edu.pl>
2020-03-29 14:15:10 +0200

    Started working on method generating surface of the moon

--- src/main/java/lunarlander/Main.java
+++ src/main/java/lunarlander/Main.java
@@ -4,18 +4,49 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import java.util.Random;
 
 public class Main extends Application {
 
+
+
   @Override
   public void start(Stage stage) {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+
+    Random rand = new Random();
+    double heightLimit = 200D;
+    Polyline polyline = new Polyline();
+
+    polyline.getPoints().addAll(new Double[]{
+      0.0, 480.0 - 25.0,
+      50.0, 480.0 - 25.0,
+      75.0, 480.0 - 75.0,
+      100.0, 480.0 - 50.0,
+      150.0, 480.0 - 50.0,
+      175.0, 480.0 - 150.0,
+      200.0, 480.0 - 25.0,
+      225.0, 480.0 - rand.nextDouble()*heightLimit,
+      250.0, 480.0 - rand.nextDouble()*heightLimit,
+      300.0, 480.0 - 100.0,
+      325.0, 480.0 - 100.0,
+      350.0, 480.0 - rand.nextDouble()*heightLimit,
+      380.0, 480.0 - rand.nextDouble()*heightLimit,
+      440.0, 480.0 - rand.nextDouble()*heightLimit,
+      490.0, 480.0 - rand.nextDouble()*heightLimit,
+      510.0, 480.0 - 150.0,
+      570.0, 480.0 - 150.0,
+      610.0, 480.0 - rand.nextDouble()*heightLimit,
+      640.0, 480.0 - rand.nextDouble()*heightLimit,
+    });
+    Group root = new Group(polyline);
+
+    Scene scene = new Scene(root, 640, 480);
     stage.setScene(scene);
     stage.show();
+
   }
 
   public static void main(String[] args) {
