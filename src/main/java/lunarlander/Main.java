c657653b2d99bf9afe1d5b58c79b9cec883cecd7
Mateusz Winicki <M.Winnicki@stud.elka.pw.edu.pl>
2020-03-31 12:32:54 +0200

    Changed polyline to polygon, added painting shape line and worked on making whole function automatized and random

--- src/main/java/lunarlander/Main.java
+++ src/main/java/lunarlander/Main.java
@@ -5,7 +5,8 @@ import javafx.scene.Scene;
 // unused
 // import javafx.scene.control.Label;
 // import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Polyline;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import java.util.Random;
@@ -19,36 +20,60 @@ public class Main extends Application {
 
     Random rand = new Random();
     double heightLimit = 200D;
-    // Polyline needs to be changed to a Polygon
-    // where two of the points are the corners of the Pane,
-    // so (0.0, pane.height) and (pane.width, pane.height)
-    Polyline polyline = new Polyline();
+
+    // Creating new object of Polygon class
+    Polygon moonSurface = new Polygon();
 
     // 480.0 needs to be changed to some sort of
-    // pane.height and [0.0, 50.0, 75.0] need to be 
+    // pane.height and [0.0, 50.0, 75.0] need to be
     // changed to [0.0*pane.width, 0.05*pane.width, ...]
-    polyline.getPoints().addAll(new Double[]{
-      0.0, 480.0 - 25.0,
-      50.0, 480.0 - 25.0,
-      75.0, 480.0 - 75.0,
-      100.0, 480.0 - 50.0,
-      150.0, 480.0 - 50.0,
-      175.0, 480.0 - 150.0,
-      200.0, 480.0 - 25.0,
-      225.0, 480.0 - rand.nextDouble()*heightLimit,
-      250.0, 480.0 - rand.nextDouble()*heightLimit,
-      300.0, 480.0 - 100.0,
-      325.0, 480.0 - 100.0,
-      350.0, 480.0 - rand.nextDouble()*heightLimit,
-      380.0, 480.0 - rand.nextDouble()*heightLimit,
+
+    double height = 480;
+    double width = 640;
+
+    // we need those values to set how favorable created
+    // moon surface will be. those 2 are only to test how it works
+    // later it will be more complicated system
+    double highVarianceWidth = 0.008 +  (0.08 - 0.08); // this one will give us
+                                                        // wider spaces
+    double lowVarianceWidth = 0.008 + (0.03 - 0.08); // and this one will give us
+                                                      // tighter spaces
+
+    double highVarianceHeight = 0.02 + (0.3125 - 0.02); // higher obstacles
+    double lowVarianceHeight = 0.02 + (0.1 - 0.02); // lower obstacles
+
+
+    // Coords of points creating our shape
+    // todo: how can i bind NEXT coord with PREVIOUS coord?
+    moonSurface.getPoints().addAll(new Double[]{
+      0.0, height,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble() * highVarianceHeight,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - 25.0,
+      rand.nextDouble() * highVarianceWidth, 480.0 - 75.0,
+      rand.nextDouble() * highVarianceWidth, 480.0 - 50.0,
+      rand.nextDouble() * highVarianceWidth, 480.0 - 50.0,
+      rand.nextDouble() * highVarianceWidth, 480.0 - 150.0,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - 25.0,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - 100.0,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - 100.0,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
+      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
       440.0, 480.0 - rand.nextDouble()*heightLimit,
       490.0, 480.0 - rand.nextDouble()*heightLimit,
       510.0, 480.0 - 150.0,
       570.0, 480.0 - 150.0,
       610.0, 480.0 - rand.nextDouble()*heightLimit,
       640.0, 480.0 - rand.nextDouble()*heightLimit,
+      width, height,
     });
-    Group root = new Group(polyline);
+
+    // create a group
+    Group root = new Group(moonSurface);
+
+    // setting color of our shape
+    moonSurface.setFill(Color.DARKBLUE);
 
     Scene scene = new Scene(root, 640, 480);
     stage.setScene(scene);
