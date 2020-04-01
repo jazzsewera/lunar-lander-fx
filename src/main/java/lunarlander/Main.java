b9b6a1cbbdea3389050e3a4bb075b1ada62b80df
Mateusz Winicki <M.Winnicki@stud.elka.pw.edu.pl>
2020-04-01 18:09:30 +0200

    Working on making pane resizable

--- src/main/java/lunarlander/Main.java
+++ src/main/java/lunarlander/Main.java
@@ -1,15 +1,18 @@
 package lunarlander;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.Scene;
 // unused
 // import javafx.scene.control.Label;
 // import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import java.util.Random;
+import javafx.beans.value.ObservableValue;
 
 public class Main extends Application {
 
@@ -69,13 +72,31 @@ public class Main extends Application {
       width, height,
     });
 
-    // create a group
-    Group root = new Group(moonSurface);
+    // create a pane (it is resizable, group is not!)
+    Pane root = new Pane(moonSurface);
 
     // setting color of our shape
     moonSurface.setFill(Color.DARKBLUE);
 
+
     Scene scene = new Scene(root, 640, 480);
+
+    root.widthProperty().addListener(new ChangeListener<Number>() {
+      @Override
+      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+        double moonSurfaceStartX = (newValue.doubleValue()/2)-(width);
+        root.setLayoutX(moonSurfaceStartX);
+      }
+    });
+
+    root.heightProperty().addListener(new ChangeListener<Number>() {
+      @Override
+      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+        double moonSurfaceStartY = (newValue.doubleValue()/2)-(height);
+        root.setLayoutY(moonSurfaceStartY);
+      }
+    });
+
     stage.setScene(scene);
     stage.show();
 
