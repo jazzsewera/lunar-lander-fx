7b92f4dc94910252151dcef78b7457d0fdd89cbb
Mateusz Winicki <M.Winnicki@stud.elka.pw.edu.pl>
2020-04-01 22:55:55 +0200

    Code for debugging sent

--- src/main/java/lunarlander/Main.java
+++ src/main/java/lunarlander/Main.java
@@ -11,66 +11,24 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+
+import java.util.Arrays;
 import java.util.Random;
 import javafx.beans.value.ObservableValue;
 
+public class Main {
+/*
 public class Main extends Application {
-
-
 
   @Override
   public void start(Stage stage) {
 
-    Random rand = new Random();
-    double heightLimit = 200D;
+    Moon moon = new Moon();
 
     // Creating new object of Polygon class
     Polygon moonSurface = new Polygon();
 
-    // 480.0 needs to be changed to some sort of
-    // pane.height and [0.0, 50.0, 75.0] need to be
-    // changed to [0.0*pane.width, 0.05*pane.width, ...]
-
-    double height = 480;
-    double width = 640;
-
-    // we need those values to set how favorable created
-    // moon surface will be. those 2 are only to test how it works
-    // later it will be more complicated system
-    double highVarianceWidth = 0.008 +  (0.08 - 0.08); // this one will give us
-                                                        // wider spaces
-    double lowVarianceWidth = 0.008 + (0.03 - 0.08); // and this one will give us
-                                                      // tighter spaces
-
-    double highVarianceHeight = 0.02 + (0.3125 - 0.02); // higher obstacles
-    double lowVarianceHeight = 0.02 + (0.1 - 0.02); // lower obstacles
-
-
-    // Coords of points creating our shape
-    // todo: how can i bind NEXT coord with PREVIOUS coord?
-    moonSurface.getPoints().addAll(new Double[]{
-      0.0, height,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble() * highVarianceHeight,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - 25.0,
-      rand.nextDouble() * highVarianceWidth, 480.0 - 75.0,
-      rand.nextDouble() * highVarianceWidth, 480.0 - 50.0,
-      rand.nextDouble() * highVarianceWidth, 480.0 - 50.0,
-      rand.nextDouble() * highVarianceWidth, 480.0 - 150.0,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - 25.0,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - 100.0,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - 100.0,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
-      rand.nextDouble() * lowVarianceWidth, 480.0 - rand.nextDouble()*heightLimit,
-      440.0, 480.0 - rand.nextDouble()*heightLimit,
-      490.0, 480.0 - rand.nextDouble()*heightLimit,
-      510.0, 480.0 - 150.0,
-      570.0, 480.0 - 150.0,
-      610.0, 480.0 - rand.nextDouble()*heightLimit,
-      640.0, 480.0 - rand.nextDouble()*heightLimit,
-      width, height,
-    });
+    moonSurface.getPoints().addAll(moon.getMoonSurfacePoints());
 
     // create a pane (it is resizable, group is not!)
     Pane root = new Pane(moonSurface);
@@ -81,6 +39,7 @@ public class Main extends Application {
 
     Scene scene = new Scene(root, 640, 480);
 
+    /*
     root.widthProperty().addListener(new ChangeListener<Number>() {
       @Override
       public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
@@ -99,11 +58,12 @@ public class Main extends Application {
 
     stage.setScene(scene);
     stage.show();
-
-  }
+  }*/
 
   public static void main(String[] args) {
-    launch();
+    //launch();
+    Moon moon = new Moon();
+    System.out.println(Arrays.toString(moon.getMoonSurfacePoints()));
   }
 
 }
--- src/main/java/lunarlander/Moon.java
+++ src/main/java/lunarlander/Moon.java
@@ -1,7 +1,49 @@
 package lunarlander;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Moon {
 
-  public Double[] recalculateWidth(double width) { }
-  public Double[] recalculateHeight(double height){ }
+  private Double[] moonSurfacePoints;
+
+  Moon() {
+    moonSurfacePoints = calculateSurfacePoints(640,480,1);
+  }
+
+  protected Double[] calculateSurfacePoints(double width, double height, int lvl) {
+    // in order to make universal array without presupposing about its size
+    // we make it from the list that we will convert to an array
+    ArrayList<Double> points = new ArrayList<Double>();
+
+    double step = 0.02 * width; // we make x coordinates based on steps
+    double maxHeight = 0.5 * height *  0.025 * lvl; // and y coordinates based on variation
+                                                    // that depends on 0.5*height
+                                                    // and lvl-based multiplication
+    Random rand = new Random();
+
+    for(double currentStep = 0; currentStep <= width; currentStep += step) {
+
+      points.add(new Double(currentStep)); // adding first coordinate (width)
+      points.add(new Double(maxHeight * rand.nextDouble()));
+    }
+
+    //change some values to the same ones todo
+
+    // we go back with List to double array, bcs polygon accepcts only that container
+    return (Double[]) points.toArray();
+  }
+
+  public Double[] getMoonSurfacePoints() {
+    return moonSurfacePoints;
+  }
+
+  public void recalculateWidth(double width) {
+    //height stays the same but width is calculated with the same step but new
+  }
+
+  public void recalculateHeight(double height) {
+  }
 }
