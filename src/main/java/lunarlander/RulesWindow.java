ad3edf38c68900e1d4a4f5d37d5b84f1cae9a164
Mateusz Winicki <M.Winnicki@stud.elka.pw.edu.pl>
2020-05-09 11:58:11 +0200

    Created a Window with set of rules described

--- src/main/java/lunarlander/RulesWindow.java
+++ src/main/java/lunarlander/RulesWindow.java
@@ -2,18 +2,45 @@ package lunarlander;
 
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 
 public class RulesWindow {
 
   public RulesWindow() {
-    this.rulesContent = new Text();
-    this.rulesContent.setText("Papież polak");
+    this.rulesFirstHeader = new Label("Rules");
+    this.rulesFirstHeader.getStyleClass().add("rules-text-primary");
+
+    this.rulesFirstParagraph = new Label("You have to touch down on the Moon with low" +
+      "\nenough velocity and angle to the ground until you" +
+      "\nrun out of fuel and Landers.");
+    this.rulesFirstParagraph.getStyleClass().add("rules-text-secondary");
+
+    this.rulesSecondHeader = new Label("Steering");
+    this.rulesSecondHeader.getStyleClass().add("rules-text-primary");
+
+    this.rulesSecondParagraph = new Label("Up Arrow - control the thrust of the Lander's engine" +
+      "\nLeft and Right Arrows - control the rotation of the Lander");
+    this.rulesSecondParagraph.getStyleClass().add("rules-text-secondary");
+
+    VBox rulesVBox = new VBox();
+    rulesVBox.getChildren().addAll(
+      rulesFirstHeader,
+      rulesFirstParagraph,
+      rulesSecondHeader,
+      rulesSecondParagraph
+    );
+    rulesVBox.getStyleClass().addAll("menu-main-vertical-layout", "menu-root");
+    rulesVBox.setAlignment(Pos.CENTER_LEFT);
+    rulesVBox.setMinWidth(400);
+    rulesVBox.setPrefWidth(750);
+    rulesVBox.setMaxWidth(1200);
+
 
     this.rulesRoot = new HBox();
     this.rulesRoot.getStyleClass().add("menu-root");
-    this.rulesRoot.getChildren().add(rulesContent);
+    this.rulesRoot.getChildren().add(rulesVBox);
     this.rulesRoot.setAlignment(Pos.CENTER);
 
     this.rulesScene = new Scene(rulesRoot);
@@ -27,6 +54,9 @@ public class RulesWindow {
 
   private Scene rulesScene;
 
-  private Text rulesContent;
+  private Label rulesFirstHeader;
+  private Label rulesFirstParagraph;
+  private Label rulesSecondHeader;
+  private Label rulesSecondParagraph;
   private HBox rulesRoot;
 }
